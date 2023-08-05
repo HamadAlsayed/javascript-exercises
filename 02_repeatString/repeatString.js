edcfd250533d1b0a8c74c23ea82108aaa21bdc8f
HamadAlsayed <hamad_sayed@outlook.com>
2023-08-05 18:38:51 +0300

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringHolder = string;
+    if (num < 0) {
+        return "ERROR";
+    } else if (num == 0) {
+        return "";
+    } else {
+        for (let i = 0; i < num - 1; i++) {
+            string = string + stringHolder;
+        }
+    }
+    
 
+    return string;
 };
 
 // Do not edit below this line
