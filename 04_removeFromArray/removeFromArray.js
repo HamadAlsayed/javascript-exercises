a4b946616e906bf86c2058bd0860fafbb4b52b6c
HamadAlsayed <hamad_sayed@outlook.com>
2023-08-07 16:01:22 +0300

    yes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
 
+    for (let i = 0; i < arr.length - 1; i++) { 
+        
+        if (arr[i] === arg[0]) {
+            arr.splice(i, 1);
+        }
+
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
