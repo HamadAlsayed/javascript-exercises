dc987022a37449f217581d6c6e1928c4cb6fac0e
HamadAlsayed <hamad_sayed@outlook.com>
2024-07-01 19:08:46 +0300

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let m;
+    let fn1 = 0,
+        fn2 = 1,
+        nextFib;
 
+    if (typeof n === "string") {
+        m = parseInt(n);
+    } else {
+        m = n;
+    }
+
+    if (m < 0) {
+        return "OOPS";
+    }
+
+    if (m == 1) {
+        return m;
+    }
+
+    for (i = 1; i < m; i++) {
+        nextFib = fn1 + fn2;
+        fn1 = fn2;
+        fn2 = nextFib;
+    }
+
+    return nextFib;
 };
 
 // Do not edit below this line
